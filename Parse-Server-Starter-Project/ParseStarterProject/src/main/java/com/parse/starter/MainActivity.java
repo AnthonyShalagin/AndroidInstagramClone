d21c3a04558c0c015fc0eb6b4598f7736c63fb03
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-01-04 18:24:45 -0500

    Added sample parse objects with queries

--- Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -14,11 +14,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 
+import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
@@ -31,6 +34,37 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+//    ParseObject score = new ParseObject("Score");   //Create Score attribute
+//    score.put("username", "anthony");
+//    score.put("score", 95);
+//    score.saveInBackground(new SaveCallback() {
+//      @Override
+//      public void done(ParseException e) {
+//
+//        if (e == null) {
+//          Log.i("SaveInBackground", "Successful ");
+//        } else {
+//          Log.i("SaveInBackground", "Failed. Error: " + e.toString());
+//        }
+//
+//      }
+//    });
+
+
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+    query.getInBackground("m0DcbMoybd", new GetCallback<ParseObject>() {
+      @Override
+      public void done(ParseObject object, ParseException e) {
+        if (e == null && object != null) {
+
+          object.put("score", 20);
+          object.saveInBackground();
+
+          Log.i("ObjectValue", object.getString("username"));
+          Log.i("ObjectValue", Integer.toString(object.getInt("score")));
+        }
+      }
+    });
     
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
