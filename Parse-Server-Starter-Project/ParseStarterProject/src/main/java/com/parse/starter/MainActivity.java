2de45f0a9cff192ca7625ce5f2b35a9a3344a06d
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-01-06 08:56:54 -0500

    Implemented signup functionality for app

--- Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -12,7 +12,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -21,16 +23,41 @@ import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 
 public class MainActivity extends AppCompatActivity {
+
+  public void signUp(View view) {
+    EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+    EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+    if (usernameEditText.getText().toString().matches("")  || passwordEditText.getText().toString().matches("")) {
+      Toast.makeText(this, "A username and password are required.", Toast.LENGTH_SHORT).show();
+    } else {
+      ParseUser user = new ParseUser();
+      user.setUsername(usernameEditText.getText().toString());
+      user.setPassword(usernameEditText.getText().toString());
+
+      user.signUpInBackground(new SignUpCallback() {
+        @Override
+        public void done(ParseException e) {
+          if (e == null) {
+            Log.i("signup", "success");
+          } else {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+          }
+        }
+      });
+    }
+
+  }
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
     
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
